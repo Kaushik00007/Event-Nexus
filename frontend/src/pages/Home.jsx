e7302042c8be8bd8b969e17b14baab9140de482d
Kaushik. K <153983811+Kaushik00007@users.noreply.github.com>
2026-03-09 12:10:14 +0530

    Update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,6 @@ import FeaturedSection from '../components/events/FeaturedSection';
 import LocalEventsSection from '../components/events/LocalEventsSection';
 import CollegeSection from '../components/colleges/CollegeSection';
 import LightRays from '../components/common/LightRays';
-import MobileCategoryGrid from '../components/events/MobileCategoryGrid';
 import { CATEGORIES } from '../utils/constants';
 import * as collegeService from '../services/collegeService';
 import * as eventService from '../services/eventService';
@@ -217,8 +216,7 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Mobile Category Grid - Unstop Style */}
-      <MobileCategoryGrid />
+
 
       {/* Featured Section with Horizontal Scrolling */}
       {featuredLoading ? (
