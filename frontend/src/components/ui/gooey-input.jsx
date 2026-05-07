bf7d889d05b8866f06bad0f8e37ae6cfea44711a
Kaushik. K <153983811+Kaushik00007@users.noreply.github.com>
2026-05-07 16:33:15 +0530

    Update

--- frontend/src/components/ui/gooey-input.jsx
+++ frontend/src/components/ui/gooey-input.jsx
@@ -69,7 +69,7 @@ export function GooeyInput({
   classNames,
   collapsedWidth = 200,
   expandedWidth = 200,
-  expandedOffset = 60,
+  expandedOffset = 50,
   gooeyBlur = 5,
   value: valueProp,
   defaultValue = "",
